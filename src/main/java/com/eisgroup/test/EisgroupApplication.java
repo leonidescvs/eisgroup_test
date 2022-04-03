6b860bb27d4b548e20d8900cccae3591dd7bfdb5
Leonides Junior <leonides.justino@gmail.com>
2022-04-03 16:11:36 -0300

    first version

---
+++ src/main/java/com/eisgroup/test/EisgroupApplication.java
@@ -0,0 +1,13 @@
+package com.eisgroup.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EisgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EisgroupApplication.class, args);
+	}
+
+}
